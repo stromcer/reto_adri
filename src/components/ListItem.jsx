16fea213b30260cc206e268b3f6416245b1bdbe7
Ignaciom87 <ignaciomaldonado9@gmail.com>
2023-03-17 19:47:07 +0000

    hecho component list

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Listltem = ({children }) => {
+
+    return (
+        <ul class="list-group">
+           {children}
+        </ul>
+
+    )
+}
+
+export default Listltem
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "./Hero";
+
+import Options from "./Options"
+
+const Main=()=> {
+    return (
+        <><Hero />
+        <Options /></>
+    )
+}
+
+export default Main
